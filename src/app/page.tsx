4926e358086a831d0463a94f792f813f97f5636f
dupcamen-dev <dupcamen-dev@github.com>
2026-09-12 11:37:15 +0300

    Move Little shop section above About us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -226,9 +226,9 @@ export default function HomePage() {
     <main className="flex-1">
       <Hero />
       <Marquee />
+      <Kramnychka />
       <Story />
       <PdfBanner />
-      <Kramnychka />
       <Gallery />
       <VisitUs />
     </main>
